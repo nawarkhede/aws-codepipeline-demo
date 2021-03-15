af6ff86bbcdd84d0ef3a71872add12ba9cc55533
nawarkhede <nishant.nawarkhede@gmail.com>
2021-03-15 12:12:42 +0530

    changes done

--- mysite/views.py
+++ mysite/views.py
@@ -2,4 +2,6 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello world from Nawarkhede, AWS is awseome infrastructure")
+    return HttpResponse(
+        "Hello world from Nawarkhede, AWS is awseome infrastructure. You should learn it."
+    )
